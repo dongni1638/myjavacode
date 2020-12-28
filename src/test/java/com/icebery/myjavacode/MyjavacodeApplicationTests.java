8fd05cb29e1feed2ceca3750cd13248e33688cf7
Icebery <dongni1638@163.com>
2020-12-28 17:10:29 +0800

    初始化项目

---
+++ src/test/java/com/icebery/myjavacode/MyjavacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebery.myjavacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjavacodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
