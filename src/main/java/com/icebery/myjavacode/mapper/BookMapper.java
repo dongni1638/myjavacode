784ff7611ae19689ffabd42de7f74712dca50563
Icebery <dongni1638@163.com>
2020-12-30 15:31:01 +0800

    调整mapper 默认修饰符

--- src/main/java/com/icebery/myjavacode/mapper/BookMapper.java
+++ src/main/java/com/icebery/myjavacode/mapper/BookMapper.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookMapper {
-    public Book getBookById(Integer id);
+    Book getBookById(Integer id);
 
 
-    public void insertBook(Book book);
+    void insertBook(Book book);
 }
